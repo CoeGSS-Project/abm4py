329b1469391e18db4dd8fa701eab5b3bb69c2ab4
ageiges <andreas.geiges@globalclimateforum.org>
2018-04-04 23:43:12 +0200

    first commit of basegraph functions

--- lib/class_graph.py
+++ lib/class_graph.py
@@ -7,6 +7,8 @@ Created on Fri Sep 15 10:31:00 2017
 """
 
 from igraph import Graph
+import numpy as np
+import itertools
 
 class GraphQueue():
 
@@ -23,7 +25,179 @@ class TypeDescription():
         self.typeIdx = nodeTypeIdx
         self.typeStr = typeStr
 
+class BaseGraph():
+    
+    CHUNK_SIZE = 1000
+    
+    def __init__(self, maxNodes, maxEdges):
+        
+        self.maxNodes       = maxNodes
+        self.maxEdges       = maxEdges
+        
+        self.freeNodeRows   = dict()
+        self.freeEdgeRows   = dict()
+        self.nodeGlob2Loc   = dict()
+        self.nodeLoc2Glob   = dict()
+        self.nAttr          = dict()
+        self.eAttr          = dict()
+        self.getNewNodeID   = dict()
+        self.getNewEdgeID   = dict()
+        
+        self.nCount   = 0
+        self.eCount   = 0
+        self.eDict    = dict() # (source, target -> leID)
+        self.edgesOut = dict() # (source -> targets)
+        self.edgesIn  = dict() # (target -> sources)
+        
+        self.getNodeTypeID = itertools.count(1).next
+        self.getEdgeTypeID = itertools.count(1).next
+        
+        #persistent nodeattributes
+        self.persNodeAttr =[('gnID', np.int64, 1)]
+        #persistent edge attributes
+        self.persEdgeAttr =[('source', np.int64, 1), 
+                            ('target', np.int64, 1)]
+    #%% NODES
+    def initNodeType(self, nodeName, size, attrDescriptor):
+        
+        nTypeID = self.getNodeTypeID()
+        dt = np.dtype(self.persNodeAttr + attrDescriptor)
+        nIDStart               = nTypeID * self.maxNodes
+        self.nAttr[nTypeID]    = np.rec.array(np.empty(size,dtype=dt))
+        self.getNewNodeID[nTypeID] = itertools.count(nIDStart).next
+        self.freeNodeRows[nTypeID] = []
+        return nTypeID
+    
+#    def extendNodeArray(self, nTypeID):
+#        currentSize = len(self.nAttr[nTypeID])
+        #self.nAttr[nTypeID][currentSize:currentSize+self.CHUNK_SIZE] = 
+        
 
+                
+    def getNodeType(self, lnID):
+        """ calculates the node type ID from local ID"""
+        return  int(lnID / self.maxNodes)
+    
+    def addNode(self, nTypeID, attributes=None):
+        
+        
+         if len(self.freeNodeRows[nTypeID]) > 0:
+             # use and old local node ID from the free row
+             dataID = self.freeNodeRows[nTypeID].pop()
+             lnID   = dataID + nTypeID * self.maxNodes
+         else:
+             # generate a new local ID
+             lnID   = self.getNewNodeID[nTypeID]()
+             #print lnID
+             dataID = lnID - nTypeID * self.maxNodes
+             #print dataID
+         dataview = self.nAttr[nTypeID][dataID:dataID+1].view() 
+         if attributes is not None:
+             dataview[:] = attributes
+         
+         return lnID, dataview
+    
+   
+
+    def remNode(self, lnID):
+        nTypeID = self.getNodeType(lnID)
+        dataID = lnID - nTypeID * self.maxNodes
+        self.freeRows.append(dataID)
+        self.nAttr[dataID]['gnID'] = -1
+    
+    def setNodeAttr(self, lnID, label, value, nTypeID=None):
+        
+        if not nTypeID:
+            nTypeID = self.getNodeType(lnID)
+        dataID = lnID - nTypeID * self.maxNodes
+        self.nAttr[nTypeID][dataID][label] = value
+    
+    def getNodeAttr(self, lnID, label=None, nTypeID=None):
+        if not nTypeID:
+            nTypeID = self.getNodeType(lnID)
+        dataID = lnID - nTypeID * self.maxNodes
+        if label:
+            return self.nAttr[nTypeID][dataID][label]
+        else:
+            return self.nAttr[nTypeID][dataID]
+        
+        
+    def setNodeSeqAttr(self, lnID, label, value, nTypeID=None):
+       
+        if not nTypeID:
+            nTypeID = self.getNodeType(lnID[0])
+        dataID = lnID - nTypeID * self.maxNodes
+        self.nAttr[nTypeID][dataID][label] = value
+    
+    def getNodeSeqAttr(self, lnIDs, label=None, nTypeID=None):
+        if not nTypeID:
+            nTypeID = self.getNodeType(lnIDs[0])
+        dataIDs = lnIDs - nTypeID * self.maxNodes
+        if label:
+            return self.nAttr[nTypeID][label][dataIDs]
+        else:
+            return self.nAttr[nTypeID][dataIDs]
+
+    #%% EDGES
+    def initEdgeType(self, nodeName, size, attrDescriptor):
+        
+        
+        eTypeID = self.getEdgeTypeID()
+        dt = np.dtype(self.persEdgeAttr + attrDescriptor)
+        self.eAttr[eTypeID]    = np.rec.array(np.empty(size,dtype=dt))
+        nIDStart               = eTypeID * self.maxNodes
+        
+        self.getNewEdgeID[eTypeID] = itertools.count(nIDStart).next
+        self.eDict[eTypeID] = dict()
+        self.edgesOut[eTypeID] = dict() 
+        self.edgesIn[eTypeID]  = dict() 
+        self.freeEdgeRows[eTypeID] = []
+        return eTypeID
+
+    def getEdgeType(self, leID):
+        """ calculates the node type ID from local ID"""
+        return  int(leID / self.maxEdges)
+    
+    def addEdge(self, eTypeID, source, target, attributes = None):
+        
+        
+         if len(self.freeEdgeRows[eTypeID]) > 0:
+             # use and old local node ID from the free row
+             dataID = self.freeEdgeRows[eTypeID].pop()
+             leID   = dataID + eTypeID * self.maxNodes
+         else:
+             # gernerate a new local ID
+             leID   = self.getNewEdgeID[eTypeID]()
+             #print lnID
+             dataID = leID - eTypeID * self.maxNodes
+             #print dataID
+         dataview = self.eAttr[eTypeID][dataID:dataID+1].view() 
+         
+         #updating edge dictionaries
+         self.eDict[eTypeID][(source, target)] = dataID
+         if source in self.edgesOut[eTypeID]:
+             self.edgesOut[eTypeID][source].append(target)
+         else:
+             self.edgesOut[eTypeID][source] = [target]
+         if target in self.edgesIn[eTypeID]:
+             self.edgesIn[eTypeID][target].append(source)
+         else:
+             self.edgesIn[eTypeID][target] = [source]     
+             
+         if attributes is not None:
+             dataview[:] = (source, target) + attributes
+         
+         return leID, dataview
+
+    def remEdge(self, leID=None, source=None, target=None):
+        
+        if leID is None:
+            leID = self.eDict((source, target))
+            
+        eTypeID = self.getEdgeType(leID)
+        self.freeEdgeRows[eTypeID].append(leID)
+        self.eAttr[leID]['source','target'] = (-1, -1)
+            
 class WorldGraph(Graph):
     """
     World graph is an agumented version of igraphs Graph that overrides some
@@ -131,3 +305,27 @@ if __name__ == "__main__":
     graph.add_vertices(5)
 
     graph.add_edges([(1,2),(2,3)], type=[1,1], weig=[0.5,0.5])
+
+
+    basegraph = BaseGraph(int(1e6), int(1e6))
+    
+    #%% nodes
+    basegraph.initNodeType('agent', 1000, [('preferences', np.float64,4)])
+    
+    lnID1, dataview1 = basegraph.addNode(1)
+    lnID2, dataview2 = basegraph.addNode(1)
+    lnID3, dataview3 = basegraph.addNode(1)
+    dataview1['gnID'] = 99
+    dataview2['gnID'] = 88
+    basegraph.getNodeAttr(lnID1)
+    print basegraph.getNodeSeqAttr(np.array([lnID1, lnID2]),'gnID')
+    
+    #%% edges
+    AGAG = basegraph.initEdgeType('ag_ag', 
+                                  1000, 
+                                  [('weig', np.float64, 1)])
+    leID1, dataview4 = basegraph.addEdge(AGAG, lnID1, lnID2, (.5,))
+    leID2, dataview4 = basegraph.addEdge(AGAG, lnID1, lnID3, (.5,))
+    dataview4
+    
+    basegraph.remEdge(leID=leID1)
